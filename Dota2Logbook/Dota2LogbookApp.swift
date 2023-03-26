2d2dc30b3102a18c8377e3d5e014ed8bf907ee84
dev_alvin <alvinmatthew12@gmail.com>
2023-03-26 18:32:44 +0700

    Initial Commit

---
+++ Dota2Logbook/Dota2LogbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dota2LogbookApp.swift
+//  Dota2Logbook
+//
+//  Created by Alvin Matthew Pratama on 26/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Dota2LogbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
